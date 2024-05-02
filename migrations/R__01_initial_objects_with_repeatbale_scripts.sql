abbb7559eab72c353a6c5705250bfab002a138c6
prashant <prashantmhatre.snf@gmail.com>
2024-05-02 20:15:09 +0100

    7

--- migrations/R__01_initial_objects_with_repeatbale_scripts.sql
+++ migrations/R__01_initial_objects_with_repeatbale_scripts.sql
@@ -3,4 +3,6 @@ select 'prasant1_1' , 'mhatre1_2'
 union all
 select 'prasant1_3' , 'mhatre1_5' 
 union all
-select 'prasant1_3' , 'mhatre1_6' ;
+select 'prasant1_3' , 'mhatre1_6' 
+union all
+select 'prasant1_7' , 'mhatre1_7' ;
